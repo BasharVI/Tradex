5cb1c9262430cbce532c5f1f897d29e6c5aff699
Bashar V I <basharvismail@gmail.com>
2023-02-14 21:36:02 +0530

    encrypted password

--- BackEnd/routes/login.js
+++ BackEnd/routes/login.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require("../db/User");
 
 router.post("/", async (req, res) => {
   if (req.body.password && req.body.email) {
-    let user = await User.findOne(req.body).select("-password");
-    if (user) {
+    const hashedPassword = bcrypt.hashSync(req.body.password, 5);
+    const data = {
+      email: req.body.email,
+      password: hashedPassword,
+    };
+    let user = await User.findOne({ data });
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
       res.send(user);
     } else {
       res.send({ result: "No User found" });
--- BackEnd/routes/signup.js
+++ BackEnd/routes/signup.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const User = require("../db/User");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  let user = new User(req.body);
+  const hashedPassword = bcrypt.hashSync(req.body.password, 5);
+  const data = {
+    username: req.body.username,
+    email: req.body.email,
+    password: hashedPassword,
+  };
+  let user = new User(data);
   let result = await user.save();
   res.send(result);
 });
